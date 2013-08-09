03a62e413d1dbc04ebac119b21c1e22b3ebd5928
Paul Gibbs <paul@byotos.com>
2013-08-09 19:39:08 +0100

    Tidy up file headers

--- extended-media-manager.php
+++ extended-media-manager.php
@@ -9,8 +9,6 @@ Text Domain: emm
 Domain Path: /languages/
 License:     GPL v2 or later
 
-Copyright © 2013 Code for the People Ltd
-
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 2 of the License, or
