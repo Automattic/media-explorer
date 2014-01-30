Automattic/media-explorer

commit 0b38e2ac172682b4f3d9e5c9cd068c792bb8350f

Fixed incorrect pagination.
Previously it was incorrectly assumed that the last id in the list would
be the `max_id` for the next query. This works for recent posts, however
for the `tags/xxx/media/recent` query this is not neccesarily true.

Instead, all Instagram endpoints return a pagination object with this
information. This change uses that information in setting `max_id` and
`min_id`.

Tags endpoint: http://instagram.com/developer/endpoints/tags/#
Pagination info: http://instagram.com/developer/endpoints/#

services/instagram/service.php
@@ -144,7 +144,7 @@ class MEXP_Instagram_Service extends MEXP_Service {
 
 		$data = array();
 		if ( 200 == $code )
-			$data = json_decode( wp_remote_retrieve_body( $response ) )->data;
+			$data = json_decode( wp_remote_retrieve_body( $response ) );
 
 		return array(
 			'code' => $code,
@@ -174,8 +174,7 @@ class MEXP_Instagram_Service extends MEXP_Service {
 
 		$response = new MEXP_Response;
 
-		foreach ( $r['data'] as $result ) {
-
+		foreach ( $r['data']->data as $result ) {
 			$item = new MEXP_Response_Item;
 
 			$item->set_id( $result->id );
@@ -199,7 +198,10 @@ class MEXP_Instagram_Service extends MEXP_Service {
 		}
 
 		// Pagination details
-		$response->add_meta( 'max_id', $result->id );
+		if ( !empty( $r['data']->pagination ) ) {
+			$response->add_meta( 'max_id', $r['data']->pagination->next_max_id );
+			$response->add_meta( 'min_id', $r['data']->pagination->next_min_id );
+		}
 
 		return $response;
 

commit 730c0b2523f3400a572ce0adbc50a5525aef12d0

Fixed incorrect reference to response data when getting user id.
0b38e2a introduced a breaking change with the format in which
`do_request()` returns data in the Instagram service. The `get_user_id()`
function never got updated with this change so it would error out when
called.

services/instagram/service.php
@@ -159,7 +159,7 @@ class MEXP_Instagram_Service extends MEXP_Service {
 		$response = $this->do_request( 'users/search', array( 'q' => $username ) );
 
 		if ( ! is_wp_error( $response ) && 200 == $response['code'] ) {
-			foreach ( $response['data'] as $user ) {
+			foreach ( $response['data']->data as $user ) {
 				if ( $user->username == $username ) {
 					return $user->id;
 				}