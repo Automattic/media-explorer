adc4ba44e4928ba82b7e8376d920dd727ae44b63
John Blackbourn <johnbillion@gmail.com>
2014-02-19 18:31:58 +0000

    Make the Keyring integration more generic, add support for Twitter connection

--- mexp-keyring-user-creds.php
+++ mexp-keyring-user-creds.php
@@ -18,20 +18,19 @@ GNU General Public License for more details.
 
 */
 
-add_filter( 'mexp_instagram_user_credentials', 'mexp_instagram_user_credentials_callback' );
-
-function mexp_instagram_user_credentials_callback( $credentials ) {
+function mexp_get_keyring_oauth2_credentials( $service, array $credentials ) {
 	
 	if ( ! class_exists( 'Keyring') ) {
 		return $credentials;
 	}
 
-	// Check that the instagram service is setup
-	$keyring = Keyring::init()->get_service_by_name( 'instagram' );
+	// Check that the service is set up
+	$keyring = Keyring::init()->get_service_by_name( $service );
+
 	if ( is_null( $keyring ) ) {
 		return $credentials;
 	}
-	
+
 	$keyring_store = Keyring::init()->get_token_store();
 	
 	// Hacky time, Keyring is designed to handle requests, but we're just stealing its access_token.
@@ -40,8 +39,8 @@ function mexp_instagram_user_credentials_callback( $credentials ) {
 		// The wpcom version uses the get_tokens_by_user method
 		$users_tokens = $keyring_store->get_tokens_by_user( get_current_user_id() );
 		
-		if ( in_array( 'instagram', $users_tokens ) ) {
-			$credentials['access_token'] = $users_tokens['instagram'][0]->token;
+		if ( isset( $users_tokens[$service] ) ) {
+			$credentials['access_token'] = $users_tokens[$service][0]->token;
 		}
 		
 	} elseif ( method_exists( $keyring_store, 'get_tokens' ) ) {
@@ -49,7 +48,7 @@ function mexp_instagram_user_credentials_callback( $credentials ) {
 		// The released version uses the get_tokens method
 		$users_tokens = $keyring_store->get_tokens(
 				array(
-					'service' => 'instagram',
+					'service' => $service,
 					'user_id' => get_current_user_id(),
 				)
 			);
@@ -62,4 +61,65 @@ function mexp_instagram_user_credentials_callback( $credentials ) {
 	
 	return $credentials;
 
+}
+
+function mexp_get_keyring_oauth1_credentials( $service, array $credentials ) {
+
+	if ( ! class_exists( 'Keyring') ) {
+		return $credentials;
+	}
+
+	// Check that the service is set up
+	$keyring = Keyring::init()->get_service_by_name( $service );
+
+	if ( is_null( $keyring ) ) {
+		return $credentials;
+	}
+
+	$keyring_store = Keyring::init()->get_token_store();
+
+	if ( method_exists( $keyring_store, 'get_tokens_by_user' ) ) {
+
+		// The wpcom version uses the get_tokens_by_user method
+		$users_tokens = $keyring_store->get_tokens_by_user( get_current_user_id() );
+
+		if ( isset( $users_tokens[$service] ) ) {
+			$token = $users_tokens[$service];
+			$credentials['consumer_key']       = $keyring->key;
+			$credentials['consumer_secret']    = $keyring->secret;
+			$credentials['oauth_token']        = $token->token->key;
+			$credentials['oauth_token_secret'] = $token->token->secret;
+		}
+
+	} elseif ( method_exists( $keyring_store, 'get_tokens' ) ) {
+
+		// The released version uses the get_tokens method
+		$users_tokens = $keyring_store->get_tokens( array(
+			'service' => $service,
+		) );
+
+		if ( count( $users_tokens ) > 0 ) {
+			$token = $users_tokens[0];
+			$credentials['consumer_key']       = $keyring->key;
+			$credentials['consumer_secret']    = $keyring->secret;
+			$credentials['oauth_token']        = $token->token->key;
+			$credentials['oauth_token_secret'] = $token->token->secret;
+		}
+
+	}
+
+	return $credentials;
+
+}
+
+add_filter( 'mexp_twitter_credentials', 'mexp_twitter_credentials_callback', 99 );
+
+function mexp_twitter_credentials_callback( array $credentials ) {
+	return mexp_get_keyring_oauth1_credentials( 'twitter', $credentials );
+}
+
+add_filter( 'mexp_instagram_user_credentials', 'mexp_instagram_user_credentials_callback', 99 );
+
+function mexp_instagram_credentials_callback( array $credentials ) {
+	return mexp_get_keyring_oauth2_credentials( 'instagram', $credentials );
 }
